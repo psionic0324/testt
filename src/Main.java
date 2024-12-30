d5bb14535e788073d8104818b089c3712f40a5d6
psionic0324 <psionic0324@naver.com>
2024-12-30 22:28:33 +0900

    main브랜치에서 수정

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     System.out.println("Hello world!");
     System.out.println("그리고 나");
     System.out.println("그리고 연습");
-    // 수정할 무언가
-    //메인 파일 수정내용
+    // aaaaaa
   }
 }
